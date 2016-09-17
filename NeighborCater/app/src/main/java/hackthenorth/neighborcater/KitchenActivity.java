08b2d5538f57a6d4a07818955c33529d30c7916d
Rowan Dempster <rowandempster@v1021-wn-195-208.campus-dynamic.uwaterloo.ca>
2016-09-17 15:06:25 -0400

    Added autocomplete for location

--- NeighborCater/app/src/main/java/hackthenorth/neighborcater/KitchenActivity.java
+++ NeighborCater/app/src/main/java/hackthenorth/neighborcater/KitchenActivity.java
@@ -3,12 +3,17 @@ package hackthenorth.neighborcater;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -24,7 +29,9 @@ public class KitchenActivity extends AppCompatActivity {
     private EditText foodName;
     private EditText foodDescription;
     private EditText kitchenName;
-    private EditText location;
+    private double lat;
+    private double longi;
+    private String address;
 
 
     private Button mSubmitButton;
@@ -39,7 +46,6 @@ public class KitchenActivity extends AppCompatActivity {
         foodName = (EditText)findViewById(R.id.editTextFoodName);
         foodDescription = (EditText)findViewById(R.id.editTextFoodDescription);
         kitchenName = (EditText) findViewById(R.id.kitchenName);
-        location = (EditText)findViewById(R.id.editTextLocation);
         mSubmitButton = (Button) findViewById(R.id.submitButton);
 
 
@@ -74,19 +80,16 @@ public class KitchenActivity extends AppCompatActivity {
                 String seekBarData = String.valueOf(seekBar.getProgress());
                 String foodDescriptionData = foodDescription.getText().toString();
                 String kitchenNameData = kitchenName.getText().toString();
-                String locationData = location.getText().toString();
 
-                double lat = 30;
-                double longt = 30;
 
                 Kitchen kObj = new Kitchen();
                 kObj.setFoodName(foodNameData);
                 kObj.setPrice(seekBarData);
                 kObj.setFoodDescription(foodDescriptionData);
                 kObj.setKitchenName(kitchenNameData);
-                kObj.setAddress(locationData);
+                kObj.setAddress(address);
                 kObj.setLatitude(lat);
-                kObj.setLongitude(longt);
+                kObj.setLongitude(longi);
                 kObj.setUser("random string that we don't know where to extract from.");
 
                 mDatabase.push().setValue(kObj);
@@ -98,7 +101,22 @@ public class KitchenActivity extends AppCompatActivity {
 
 
 
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.editTextLocation);
 
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                lat = place.getLatLng().latitude;
+                longi = place.getLatLng().longitude;
+                address = place.getAddress().toString();
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+            }
+        });
 
 
 
