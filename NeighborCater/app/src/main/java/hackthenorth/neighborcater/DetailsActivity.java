8cf52bffca1dac4de4737e7b00cff6b9c38e4daa
Rowan Dempster <rowandempster@v1021-wn-195-208.campus-dynamic.uwaterloo.ca>
2016-09-17 23:45:40 -0400

    Added xe currency exchange values

--- NeighborCater/app/src/main/java/hackthenorth/neighborcater/DetailsActivity.java
+++ NeighborCater/app/src/main/java/hackthenorth/neighborcater/DetailsActivity.java
@@ -1,10 +1,13 @@
 package hackthenorth.neighborcater;
 
+import android.app.DownloadManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -13,11 +16,27 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 import hackthenorth.neighborcater.models.Kitchen;
 import hackthenorth.neighborcater.models.User;
+import hackthenorth.neighborcater.models.XEResponse;
+import hackthenorth.neighborcater.models.XETo;
+import hackthenorth.neighborcater.networking.FoodService;
+import hackthenorth.neighborcater.networking.ServiceGenerator;
+import hackthenorth.neighborcater.utils.RoundingUtils;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Observable;
+import rx.Subscriber;
+import rx.schedulers.Schedulers;
 
 /**
  * Created by rowandempster on 9/17/16.
@@ -27,6 +46,7 @@ public class DetailsActivity extends AppCompatActivity {
     Kitchen kitchen;
     User user;
     private DatabaseReference mDatabase;
+    Retrofit retrofit;
 
     ValueEventListener postListener = new ValueEventListener() {
         @Override
@@ -54,8 +74,11 @@ public class DetailsActivity extends AppCompatActivity {
         mDatabase.addValueEventListener(postListener);
         kitchen = (Kitchen) getIntent().getSerializableExtra("Kitchen");
 
-
-
+        retrofit = new Retrofit.Builder()
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl("https://xecdapi.xe.com/v1/convert_from.json/")
+                .build();
 
 
 
@@ -81,5 +104,52 @@ public class DetailsActivity extends AppCompatActivity {
         email.setText("Email: " + user.getEmail());
         phoneAndDistance.setText(user.getNumber() + " | " + getIntent().getSerializableExtra("Distance") + " km");
         price.setText("$"+kitchen.getPrice());
+        price.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FoodService service = ServiceGenerator.createService(FoodService.class, "hackthenorth012", "Waterloo3043");
+
+                Call<XEResponse> xeResponseObservable = service.getXETest("USD,EUR,JPY,GBP,AUD,CAD,CHF,CNY,MXC,INR", "CAD", kitchen.getPrice());
+                xeResponseObservable.enqueue(new Callback<XEResponse>() {
+                    @Override
+                    public void onResponse(Call<XEResponse> call, Response<XEResponse> response) {
+                        populateCurrency((XEResponse)response.body());
+                    }
+
+                    @Override
+                    public void onFailure(Call<XEResponse> call, Throwable t) {
+                        Log.d("asdf", t.getMessage());
+                    }
+                });
+
+            }
+        });
     }
+
+    private void populateCurrency(final XEResponse xeResponse) {
+        this.runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                LinearLayout xeContainer = (LinearLayout) DetailsActivity.this.findViewById(R.id.xe_container);
+                xeContainer.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        view.setVisibility(View.GONE);
+                    }
+                });
+                xeContainer.removeAllViews();
+                XETo[] toArray = xeResponse.getTo();
+                for (XETo xeTo : toArray){
+                    View xeView = getLayoutInflater().inflate(R.layout.xe_cell, null);
+                    ((TextView)(xeView.findViewById(R.id.xe_cell_currency))).setText(xeTo.getQuotecurrency());
+                    double amount = Double.valueOf(xeTo.getMid());
+                    ((TextView)(xeView.findViewById(R.id.xe_cell_amount))).setText(RoundingUtils.roundDoubleToTwoDecimals(amount));
+                    xeContainer.addView(xeView);
+                }
+                xeContainer.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+
+
 }
